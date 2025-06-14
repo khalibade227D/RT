a8359a36e644baaa32e7c7bb1b8e922efefb8088
khalifa <khalibade8@gmail.com>
2025-06-14 12:36:50 -0700

    1.6

--- app/src/main/java/com/example/rt/Activity/TicketDetailActivity.java
+++ app/src/main/java/com/example/rt/Activity/TicketDetailActivity.java
@@ -1,14 +1,33 @@
 package com.example.rt.Activity;
-
 import android.os.Bundle;
 import android.view.View;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.pdf.PdfDocument;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.example.rt.databinding.ActivityTicketDetailBinding;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import com.bumptech.glide.Glide;
 import com.example.rt.Model.Flight;
 import com.example.rt.R;
@@ -16,6 +35,7 @@ import com.example.rt.databinding.ActivityTicketDetailBinding;
 
 public class TicketDetailActivity extends BaseActivity2 {
     private ActivityTicketDetailBinding binding;
+
     private Flight flight;
 
 
